751b750356a7c98ac98fe3cf6a6dcbf87fdcf33d
Abhaykrb <65072557+Abhaykrb@users.noreply.github.com>
2021-07-06 23:19:40 -0700

    Add files via upload

---
+++ locallibrary/views.py
@@ -0,0 +1,55 @@
+from django.views import generic
+from .models import Book, Author, BookInstance, Genre
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_genre = Genre.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(
+        status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genre': num_genre,
+        'num_visits': num_visits,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+# Django default generic view willread  Book model objects then renders "book_list.html
+
+
+class BookListView(generic.ListView):
+    model = Book
+# itmeeansonly 2itemswill be shown then more than that will be shown atnext page
+    paginate_by = 2
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
